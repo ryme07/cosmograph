1fea27fc8cf8bf39c2303b3015cacd6a5ee1f8af
Rémy <remy.delrieu@eulerts.com>
2023-09-01 13:58:47 +0200

    try new graph

--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,55 @@
-import "./styles.css";
-import { nodes, links, Node, Link } from "./data-gen";
 import { Graph, GraphConfigInterface } from "@cosmograph/cosmos";
 
+import { nodes, links, Node, Link } from "./data";
+import { CosmosLabels } from "./labels";
+
+import "./styles.css";
+
 const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+const div = document.querySelector("#labels") as HTMLDivElement;
+
+const cosmosLabels = new CosmosLabels<Node, Link>(div);
+const nodesToShowLabelsFor = [
+  "Drury Lane Theatre",
+  "King's Theatre",
+  "Lincoln's Inn Fields",
+  "Goodman's Fields",
+  "Haymarket Theatre",
+  "Covent Garden",
+  "Bartholomew Fair",
+  "Southwark Fair",
+  "Pantheon, Oxford Street",
+];
+
 let graph: Graph<Node, Link>;
-const config: GraphConfigInterface<Node, Link> = {
+export const config: GraphConfigInterface<Node, Link> = {
   backgroundColor: "#151515",
-  nodeSize: 4,
-  nodeColor: "#4B5BBF",
+  nodeSize: (n) => (nodesToShowLabelsFor.includes(n.id) ? 8 : 2),
+  nodeColor: (n) => n.color,
   nodeGreyoutOpacity: 0.1,
   linkWidth: 0.1,
   linkColor: "#5F74C2",
   linkArrows: false,
   linkGreyoutOpacity: 0,
   simulation: {
-    linkDistance: 1,
-    linkSpring: 2,
-    repulsion: 0.2,
     gravity: 0.1,
-    decay: 100000,
+    linkDistance: 1,
+    linkSpring: 0.3,
+    repulsion: 0.4,
+    onTick: () => cosmosLabels.update(graph),
   },
   events: {
-    onClick: (node, i, pos, event) => {
-      if (node && i !== undefined) {
-        graph.selectNodeByIndex(i);
-        graph.zoomToNodeByIndex(i);
-      } else {
-        graph.unselectNodes();
-      }
-      console.log("Clicked node: ", node);
-    },
+    onZoom: () => cosmosLabels.update(graph),
   },
 };
 
 graph = new Graph(canvas, config);
 graph.setData(nodes, links);
-graph.zoom(0.9);
+graph.setZoomLevel(0.6);
 
-/* ~ Demo Actions ~ */
-// Start / Pause
-let isPaused = false;
-const pauseButton = document.getElementById("pause") as HTMLDivElement;
-
-function pause() {
-  isPaused = true;
-  pauseButton.textContent = "Start";
-  graph.pause();
-}
-
-function start() {
-  isPaused = false;
-  pauseButton.textContent = "Pause";
-  graph.start();
-}
-
-function togglePause() {
-  if (isPaused) start();
-  else pause();
-}
-
-pauseButton.addEventListener("click", togglePause);
-
-// Zoom and Select
-function getRandomNodeId() {
-  return nodes[Math.floor(Math.random() * nodes.length)].id;
-}
-
-function getRandomInRange([min, max]: [number, number]): number {
-  return Math.random() * (max - min) + min;
-}
-
-function fitView() {
-  graph.fitView();
-}
-
-function zoomIn() {
-  const nodeId = getRandomNodeId();
-  graph.zoomToNodeById(nodeId);
-  graph.selectNodeById(nodeId);
-  pause();
-}
-
-function selectPoint() {
-  const nodeId = getRandomNodeId();
-  graph.selectNodeById(nodeId);
-  graph.fitView();
-  pause();
-}
-
-function selectPointsInArea() {
-  const w = canvas.clientWidth;
-  const h = canvas.clientHeight;
-  const left = getRandomInRange([w / 4, w / 2]);
-  const right = getRandomInRange([left, (w * 3) / 4]);
-  const top = getRandomInRange([h / 4, h / 2]);
-  const bottom = getRandomInRange([top, (h * 3) / 4]);
-  pause();
-  graph.selectNodesInRange([
-    [left, top],
-    [right, bottom],
-  ]);
-}
-
-document.getElementById("fit-view")?.addEventListener("click", fitView);
-document.getElementById("zoom")?.addEventListener("click", zoomIn);
-document.getElementById("select-point")?.addEventListener("click", selectPoint);
-document
-  .getElementById("select-points-in-area")
-  ?.addEventListener("click", selectPointsInArea);
+// _Track the nodes_ for which you wish to display labels.
+// Their coordinates in the simulation space will be accessible
+// via the `getTrackedNodePositionsMap` method. You can then convert
+// them to the screen space with the `spaceToScreenPosition`
+// method.
+graph.trackNodePositionsByIds(nodesToShowLabelsFor);
